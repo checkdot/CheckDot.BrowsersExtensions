96ea137c7f40f2702762770c7cc9dc51f2e87e4a
Ihor Siryi <ihorsiryi@gmail.com>
2023-03-31 15:35:52 +0300

    make it pretty view

--- extension/coingecko.js
+++ extension/coingecko.js
@@ -14,14 +14,26 @@ async function coingeckoHandler() {
     if (riskScoreItem) {
         let style = getStyleForCoinGeckoScore(riskScoreItem.score)
 
-        const insertHTML = `<div class="sc-aef7b723-0 cevGxl" display="flex" style="flex-wrap:wrap; margin-bottom: 10px;">
-                                <a target="_blank" href="${buildTrackerURL(riskScoreItem)}"><div class="namePill namePillPrimary"
-                                     style="${style}">Trust score: <span style="font-weight: bold;">${riskScoreItem.score}</span>
-                                </div></a>
-                                <div class="namePill">Powered by <a target="_blank" href="https://checkdot.io/">Checkdot.io</a></div>
-                            </div>`
+        const insertHTML = `<div class="tw-flex tw-items-center tw-mt-3">
+                            <div style="" data-controller="">
+                                <div data-gecko-primer--tooltip-target="anchor" data-action="mouseenter->gecko-primer--tooltip#show"
+                                     aria-describedby="tooltip" class="tw-inline-block">
+                                    <a target="_blank" href="${buildTrackerURL(riskScoreItem)}">
+                                        <button class="btn btn-sm tw-outline-btn-md center tw-p-0 tw-w-12 tw-mr-1"
+                                                style="${style}; width: 110px;">Trust
+                                            score: &nbsp;<span style="font-weight: bold;">${riskScoreItem.score}</span>
+                                        </button>
+                                    </a>
+                                </div>
+                            </div>
+                            <div
+                                class="tw-block tw-inline-flex tw-py-0.5 tw-h-5 tw-items-center tw-px-2 tw-rounded-md tw-text-xs tw-font-medium tw-bg-gray-100 tw-text-gray-800 tw-mb-1 md:tw-mb-0 dark:tw-text-white dark:tw-bg-white dark:tw-bg-opacity-5">
+                                <i class="far fas tw-mr-0.5 star-color fa-star"></i>Powered by &nbsp;<a target="_blank"
+                                                                                                            href="https://checkdot.io/">Checkdot.io</a>
+                            </div>
+                        </div>`
 
-        $('[data-target="coins-information.mobileOptionalInfo"]').before($.parseHTML(insertHTML));
+        $('[data-controller="coin-add-this"]').parent().parent().parent().parent().before($.parseHTML(insertHTML));
 
     } else {
         console.log(`risk tracker data not found for ${projectTicker.toUpperCase()} and ${projectName} on checkdot.io`);
@@ -45,4 +57,3 @@ function getStyleForCoinGeckoScore(score) {
 
     return style;
 }
-
